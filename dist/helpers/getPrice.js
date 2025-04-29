852234c347b8e112be681211f78598ef96b247aa
Bepis1559 <axl2001@abv.bg>
2025-04-29 15:57:56 +0300

    removed src and added the built files

---
+++ dist/helpers/getPrice.js
@@ -0,0 +1,14 @@
+import { getCurrentSlot } from "./getCurrentSlot.js";
+export async function getPrice() {
+  const response = await fetch(
+    "https://ibex.bg/Ext/IDM_Homepage/fetch.php?lang=bg",
+  );
+  const data = await response.json();
+  const intradayArray = data[3];
+  const todaysDate = new Date().toISOString().split("T")[0];
+  const currentSlot = getCurrentSlot();
+  const targetDateTime = `${todaysDate} ${currentSlot}`;
+  const foundData = intradayArray.find(({ date }) => date === targetDateTime);
+  const price = foundData ? foundData.price : null;
+  return price;
+}
---
+++ dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+// import { handleFetching } from "./helpers/handleFetching";
+// async function main() {
+//   let price = await handleFetching();
+//   setInterval(async () => {
+//     price = await handleFetching();
+//   }, 300000);
+//   return price;
+// }
+// await main();
