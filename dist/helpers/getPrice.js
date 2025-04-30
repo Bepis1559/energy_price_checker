677d1d26ac0e343a607c1dca77995caeb5ff568b
Bepis1559 <axl2001@abv.bg>
2025-04-30 08:27:51 +0300

    updated imports , so they have .js file extension and removed index.js

--- dist/helpers/getPrice.js
+++ dist/helpers/getPrice.js
@@ -1,10 +1,12 @@
-import { getCurrentSlot } from "./getCurrentSlot";
+import { getCurrentSlot } from "./getCurrentSlot.js";
 export async function getPrice() {
-    const response = await fetch("https://ibex.bg/Ext/IDM_Homepage/fetch.php?lang=bg");
-    const data = await response.json();
-    const intradayArray = data[3];
-    const targetDateTime = getCurrentSlot();
-    const foundData = intradayArray.find(({ date }) => date === targetDateTime);
-    const price = foundData ? foundData.price : null;
-    return price;
+  const response = await fetch(
+    "https://ibex.bg/Ext/IDM_Homepage/fetch.php?lang=bg",
+  );
+  const data = await response.json();
+  const intradayArray = data[3];
+  const targetDateTime = getCurrentSlot();
+  const foundData = intradayArray.find(({ date }) => date === targetDateTime);
+  const price = foundData ? foundData.price : null;
+  return price;
 }
